2e57994728016e150b65dc9fffcd029d3e5d4530
returnandrisk <returnandrisk@users.noreply.github.com>
2015-01-21 01:24:47 +0800

    update license

--- FOMC Dates - Price Data Exploration.R
+++ FOMC Dates - Price Data Exploration.R
@@ -1,7 +1,21 @@
 ################################################################################
-# R code for 14 Dec 2014 post on www.returnandrisk.com:
 # FOMC Dates - Price Data Exploration
+# R code for 14 Dec 2014 post on www.returnandrisk.com:
 # http://www.returnandrisk.com/2014/12/fomc-dates-price-data-exploration.html
+# Copyright (C) 2014  Peter Chan (peter-at-return-and-risk-dot-com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 install.packages(c("quantmod", "reshape2", "ggplot2"), repos = "http://cran.us.r-project.org")
--- FOMC Dates - Scraping Data From Web Pages.R
+++ FOMC Dates - Scraping Data From Web Pages.R
@@ -1,7 +1,21 @@
 ################################################################################
-# R code for 30 Nov 2014 post on www.returnandrisk.com:
 # FOMC Dates - Scraping Data From Web Pages
+# R code for 30 Nov 2014 post on www.returnandrisk.com:
 # http://www.returnandrisk.com/2014/11/scraping-data-from-web-pages-fomc-dates.html
+# Copyright (C) 2014  Peter Chan (peter-at-return-and-risk-dot-com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 install.packages(c("httr", "XML"), repos = "http://cran.us.r-project.org")
